5141ff5e645c7a9a2ed938d1940dd3dc93007d14
Vincent <vincentliu362@gmail.com>
2023-09-13 12:42:18 -0500

    init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <NuxtLayout name="header">
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+let name = ref("Vincent");
+</script>
+
+<template>
+  <div>current name: {{ name }}</div>
+  <div>
+    <input type="enter name" v-model="name" />
+  </div>
+</template>
+
+<style scoped></style>
