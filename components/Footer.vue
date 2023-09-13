564382d0d5223dc25e23fb8a9c1100ea6fc322cd
Vincent <vincentliu362@gmail.com>
2023-09-13 13:03:12 -0500

    refactor: add components

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <a href="https://youtube.com">Your Website</a>
+</template>
+
+<style scoped></style>
---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/other">Other</NuxtLink>
+  </nav>
+</template>
+
+<style scoped></style>
